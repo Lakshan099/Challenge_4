e3e4e7beeff266431c85b63bde6afb1e6a6ab04c
Lakshan099 <lakshan.rsandanayaka@gmail.com>
2022-12-22 22:38:39 +0530

    create student class

---
+++ Student.java
@@ -0,0 +1,40 @@
+public class Student {
+    private String fName;
+    private String lName;
+    private String course;
+
+    public Student(String fName, String lName, String course) {
+        this.fName = fName;
+        this.lName = lName;
+        this.course = course;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    
+
+    
+
+}
